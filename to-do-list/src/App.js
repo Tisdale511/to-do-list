baabeba8f04da80db375170b60fdd33f16551e54
Tisdale511 <tisdalefry@gmail.com>
2021-03-23 15:33:23 -0400

    server is working

--- to-do-list/src/App.js
+++ to-do-list/src/App.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import './App.css';
 import List from './List';
-import Task from './Task';
+// import Task from './Task';
 import NewTask from './NewTask';
 
 function App() {
 
-  const [tasks, setTasks] = setState([])
+  const [tasks, setTasks] = React.useState([])
 
   const addTask = taskName => {
     setTasks(oldTaskList => [...oldTaskList, taskName])
@@ -14,9 +14,11 @@ function App() {
 
 
   return (
-    <List tasks={tasks}/>,
-    <Task addTask={addTask} />,
-    <NewTask addTask={addTask} />
+    <div>hello world
+      <List tasks={tasks}/>,
+      {/* <Task addTask={addTask} />, */}
+      <NewTask addTask={addTask} />
+    </div>  
   );
 }
 
