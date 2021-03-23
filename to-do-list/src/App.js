be88db99b8c2c9215667f5701bf18d4eed1bd0e6
Tisdale511 <tisdalefry@gmail.com>
2021-03-23 15:16:36 -0400

    created task setSate

--- to-do-list/src/App.js
+++ to-do-list/src/App.js
@@ -1,8 +1,15 @@
+import React from 'react';
 import './App.css';
-import List from './List'
-import Task from './Task'
+import List from './List';
+import Task from './Task';
 
 function App() {
+
+  const [tasks, setTasks] = setState([])
+
+
+
+
   return (
     <List />,
     <Task />
