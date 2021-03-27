81db82998b29770b5b75251563d744c0ee755506
Tisdale511 <tisdalefry@gmail.com>
2021-03-27 17:28:25 -0400

    Header imported to App component

--- to-do-list/src/App.js
+++ to-do-list/src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import List from './List';
 // import Task from './Task';
 import NewTask from './NewTask';
+import Header from './Header'
 
 function App() {
 
@@ -17,6 +18,7 @@ function App() {
     <div>hello world
       <List tasks={tasks}/>,
       {/* <Task addTask={addTask} />, */}
+      {/* <Header/>, */}
       <NewTask addTask={addTask} />
     </div>  
   );
